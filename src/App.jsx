5e796d309b594968bfd9e275a5736a494df4954c
Blaise202 <iblaise2023@gmail.com>
2025-10-29 08:34:59 +0000

    lists and conditionals

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,30 @@ import "./App.css";
 import Button from "./Button.jsx";
 import ButtonSection from "./ButtonSection.jsx";
 import Textbox from "./Textbox.jsx";
+import { TextBlock } from "./TextBlock.jsx";
+import { StoreItem } from "./StoreItem.jsx";
+import { useState } from "react";
 
 function App() {
+  const item1 = { title: "Gloves", description: "Footbal Gloves", price: 100 };
+  const item2 = { title: "Ball", description: "Footbal Ball", price: 1000 };
+
+  function HandleShow({ show }) {
+    if (show) {
+      return <StoreItem item={item1} />;
+    }
+  }
+
+  const [showItem, setShowItem] = useState(false);
+
   return (
     <>
-      <Button />
+      <Button
+        show={showItem}
+        setShow={setShowItem}
+      />
+      <HandleShow show={showItem} />
+      {showItem && <StoreItem item={item2} />}
     </>
   );
 }
--- src/Button.jsx
+++ src/Button.jsx
@@ -1,36 +1,9 @@
-import { useState, useEffect } from "react";
-
-function Button() {
-  const [count, setCount] = useState(0);
-  const [count2, setCount2] = useState(0);
-  const [filter, setFilter] = useState("None");
-
-  useEffect(() => {
-    const colors = [
-      "red",
-      "blue",
-      "black",
-      "cyan",
-      "green",
-      "purple",
-      "white",
-      "grey",
-    ];
-    console.log("count has changed");
-    document.body.style.backgroundColor = colors[Math.round(Math.random() * 7)];
-    // setFilter(filter + "Yo");
-    // console.log(filter);
-  }, [count, filter]);
-
-  useEffect(() => {
-    console.log("we ain't changing nothing");
-    // setCount2(count2 + 1);
-  }, [count2]);
-
+function Button({ show, setShow }) {
   return (
     <>
-      <button onClick={() => setCount(count + 1)}>{count}</button>
-      <button onClick={() => setCount2(count2 + 1)}>{count2}</button>
+      <button onClick={() => setShow(!show)}>
+        {show ? "Hide Button" : "Show Button"}
+      </button>
     </>
   );
 }
